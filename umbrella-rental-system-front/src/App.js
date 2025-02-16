29b6594b9852a4bd3b83376c18ec4f8d491c49f4
kure <2312110009y@kindai.ac.jp>
2025-02-16 17:21:00 +0900

    傘貸しシステムのフロントのひな形を作成

---
+++ umbrella-rental-system-front/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <span class="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
+      HelloWorld
+    </span>
+  );
+}
+
+export default App;
